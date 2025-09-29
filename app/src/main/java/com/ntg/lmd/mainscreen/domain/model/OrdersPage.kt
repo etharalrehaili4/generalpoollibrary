1f48e145b857df199996bd24671a6e93612ea70d
ealrehaili <ealrehaili@ntgclarity.com>
2025-09-29 18:28:24 +0300

    remove app

--- app/src/main/java/com/ntg/lmd/mainscreen/domain/model/OrdersPage.kt
+++
@@ -1,8 +0,0 @@
-package com.ntg.lmd.mainscreen.domain.model
-
-import com.example.lmdmypoolmyorder.domain.model.OrderInfo
-
-data class OrdersPage(
-    val items: List<OrderInfo>,
-    val rawCount: Int, // server page size BEFORE filtering
-)
