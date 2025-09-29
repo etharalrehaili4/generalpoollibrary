b7542a81a9e5bb48b5748be08df4836b7db539f9
etharalrehaili4 <ealrehaili@ntgclarity.com>
2025-09-29 21:48:39 +0300

    Update build.gradle.kts

--- generalpool/build.gradle.kts
+++ generalpool/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("maven-publish")
 }
 
 android {
@@ -35,17 +34,6 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            afterEvaluate { from(components["release"]) }
-            groupId = "com.github.etharalrehaili4"
-            artifactId = "generalpool" 
-            version = "1.0.0"
-        }
     }
 }
 
